7706e288f938a2f0e8f059edfd529668149cdc76
Benjamin Cabé <kartben@gmail.com>
2016-01-08 13:48:32 +0100

    fix unzip on non-linux

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ echo '** Downloading external files (if needed) **'
 
 wget -N https://hudson.eclipse.org/leshan/job/leshan/lastSuccessfulBuild/artifact/leshan-standalone.jar -P external
 wget -N https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-demo.zip -P external
-unzip external/openhab-2.0.0.alpha2-demo.zip -d external/openhab -u
+unzip -u external/openhab-2.0.0.alpha2-demo.zip -d external/openhab 
 
 if [ ! -f "external/wakaama/lwm2mclient" ]; then
   echo '** Compiling Wakaama **'
