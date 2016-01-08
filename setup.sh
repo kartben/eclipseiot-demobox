39b646833883fb91dad6b4ba32a31e366bba877c
Benjamin Cabé <benjamin@eclipse.org>
2016-01-08 17:41:44 +0100

    update openhab demo link

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ npm install
 echo '** Downloading external files (if needed) **'
 
 wget -N https://hudson.eclipse.org/leshan/job/leshan/lastSuccessfulBuild/artifact/leshan-standalone.jar -P external
-wget -N https://bintray.com/artifact/download/openhab/bin/openhab-2.0.0.alpha2-demo.zip -P external
-unzip -u external/openhab-2.0.0.alpha2-demo.zip -d external/openhab 
+wget -N https://openhab.ci.cloudbees.com/job/openHAB2/lastSuccessfulBuild/artifact/distribution/target/distribution-2.0.0-SNAPSHOT-demo.zip -P external
+unzip -u external/distribution-2.0.0-SNAPSHOT-demo.zip -d external/openhab 
 
 if [ ! -f "external/wakaama/lwm2mclient" ]; then
   echo '** Compiling Wakaama **'
