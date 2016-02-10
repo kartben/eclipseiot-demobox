aa45fa9651bfcb51d695f195ad6b76916777b052
Benjamin Cabé <kartben@gmail.com>
2016-02-10 12:22:21 +0100

    Build tiaki at setup time

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-npm install
+npm install --loglevel info
 
 echo '** Downloading external files (if needed) **'
 
 wget -N https://hudson.eclipse.org/leshan/job/leshan/lastSuccessfulBuild/artifact/leshan-standalone.jar -P external
 wget -N https://openhab.ci.cloudbees.com/job/openHAB-Distribution/lastSuccessfulBuild/artifact/distributions/openhab-offline/target/openhab-offline-2.0.0-SNAPSHOT.zip -P external
-unzip -u external/openhab-offline-2.0.0-SNAPSHOT.zip -d external/openhab 
+unzip -u external/openhab-offline-2.0.0-SNAPSHOT.zip -d external/openhab
 
 if [ ! -f "external/wakaama/lwm2mclient" ]; then
   echo '** Compiling Wakaama **'
@@ -15,6 +15,16 @@ if [ ! -f "external/wakaama/lwm2mclient" ]; then
   make -j4
   cd ../..
 fi
+
+if [ ! -f "external/tiaki-java/service-discovery-cli/build/libs/tiaki-cli-1.0.jar" ]; then
+  echo '** Compiling Tiaki **'
+  cd external/tiaki-java/service-discovery-cli
+  ./gradlew clean fatJar
+  cd ../../..
+fi
+
+
+
 
 screen -c .screenrc
 
